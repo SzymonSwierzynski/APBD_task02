cd892badf83b52845adeb23199760784040c2604
Szymon Świerzyński <s33102@pjwstk.edu.pl>
2026-03-24 13:34:27 +0100

    added IRentalService interface

--- Interfaces/IRentalService.cs
+++ Interfaces/IRentalService.cs
@@ -1,6 +1,15 @@
+using APBD_TASK2.Models;
+
 namespace APBD_TASK2.Interfaces;
 
 public interface IRentalService
 {
-    
+    void AddUser(User user);
+    void AddEquipment(Equipment equipment);
+
+    List<Equipment> GetAllEquipment();
+    List<Equipment> GetAvailableEquipment();
+
+    void RentEquipment(int userId, int equipmentId, int days);
+    void ReturnEquipment(int equipmentId);
 }
