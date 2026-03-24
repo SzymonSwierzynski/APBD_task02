e9e4d179276ab7d68e1e0a393605a70f94ee86d5
Szymon Świerzyński <s33102@pjwstk.edu.pl>
2026-03-24 10:59:31 +0100

    Add basic service methods for users and equipment

--- Services/RentalService.cs
+++ Services/RentalService.cs
@@ -1,6 +1,21 @@
+using APBD_TASK2.Interfaces;
+using APBD_TASK2.Models;
+
 namespace APBD_TASK2.Services;
 
-public class RentalService 
+public class RentalService : IRentalService
 {
-    
+    private List<User> _users = new();
+    private List<Equipment> _equipment = new();
+    private List<Rental> _rentals = new();
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipment.Add(equipment);
+    }
 }
